df36ab81f1b7ff3478526f4d68244584f481cc94
JiangJian <1858008535@qq.com>
2022-03-10 04:33:24 -0800

    Delete verdi log

--- base/sync_fifo/sim/verdiLog/verdi.cmd
+++
@@ -1,5 +0,0 @@
-debImport "/home/soke/ICdesign/base/sync_fifo/sim/../rtl/sync_fifo.v" \
-          "/home/soke/ICdesign/base/sync_fifo/sim/../tb/sync_fifo_tb.v"
-wvCreateWindow
-wvOpenFile -win $_nWave2 {/home/soke/ICdesign/base/sync_fifo/sim/tb.fsdb}
-debExit
