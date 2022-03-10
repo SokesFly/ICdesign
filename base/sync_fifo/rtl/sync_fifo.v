b6baaac88166577b2a2ab58bfc45fab7cba05a76
JiangJian <1858008535@qq.com>
2022-03-10 04:31:46 -0800

    (New): Add sync_fifo

---
+++ base/sync_fifo/rtl/sync_fifo.v
@@ -0,0 +1,16 @@
+/***************************************************************************************
+* Function: 
+* Author: SK 
+* Company: Ltd.JRLC.SK
+* Right : 
+* Tel : 
+* Last modified: 2022-03-10 04:03
+* None: 
+* Filename: sync_fifo.v
+* Resverd: 
+* Description: 
+**************************************************************************************/
+
+module sync_fifo();
+
+endmodule
---
+++ base/sync_fifo/sim/verdiLog/verdi.cmd
@@ -0,0 +1,5 @@
+debImport "/home/soke/ICdesign/base/sync_fifo/sim/../rtl/sync_fifo.v" \
+          "/home/soke/ICdesign/base/sync_fifo/sim/../tb/sync_fifo_tb.v"
+wvCreateWindow
+wvOpenFile -win $_nWave2 {/home/soke/ICdesign/base/sync_fifo/sim/tb.fsdb}
+debExit
