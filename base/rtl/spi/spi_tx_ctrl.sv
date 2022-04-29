0f5eb620e1e555933c7c88fe4f8f883bc8b00b1d
soke <jiangjian@cdjrlc.com>
2022-04-29 01:47:50 -0700

    Add spi

--- base/rtl/spi/spi_tx_ctrl.sv
+++ base/rtl/spi/spi_tx_ctrl.sv
@@ -26,14 +26,19 @@ module                                  spi_tx_ctrl #(
     output wire                         tx_ctrl_eot_o   , // This task transmit done
     );
 
-typedef enum bit [2:0] {
+typedef enum bit [3:0] {
     SPI_TX_IDLE,
+    SPI_TX_READY,
     SPI_TX_TRANSMIT,
     SPI_TX_EOT
     } tx_ctrl_fsm_cs, tx_ctrl_fsm_ns;
 
 // Decalre inner register for stat length has been transmit
 reg  [31            :0]                 length_send_cnt ;
+
+// Read data from fifo 
+reg   [SPI_TX_DWIDE-1:0]                rd_data_r       ;
+reg                                     rd_en_r         ;
 
 // Declare spi tx phy signals
 reg   [SPI_TX_DWIDE-1:0]                tx_data_r       ;
@@ -45,8 +50,12 @@ wire                                    tx_rdy          ;
 
 // TX-FSM status controll signals
 wire                                    to_spi_tx_idle      ;
+wire                                    to_spi_tx_ready     ;
 wire                                    to_spi_tx_transmit  ;
 wire                                    to_spi_tx_eot       ;
+
+// assign fifo read signals
+assign                                  rd_en_i = rd_en_r ;
 
 // TX-FSM status transfer
 always@(posedge clk_i or negedge rstn_i) begin
@@ -59,7 +68,8 @@ always@(posedge clk_i or negedge rstn_i) begin
 end
 
 // TX-FSM status jump
-assign          to_spi_tx_transmit = (tx_ctrl_fsm_cs == SPI_TX_IDLE) && tx_ctrl_st_i;
+assign          to_spi_tx_ready    = (tx_ctrl_fsm_cs == SPI_TX_IDLE) && tx_ctrl_st_i && tx_rdy;
+assign          to_spi_tx_transmit = (tx_ctrl_fsm_cs == SPI_TX_READY);
 assign          to_spi_tx_eot      = (tx_ctrl_fsm_cs == SPI_TX_TRANSMIT) && (transmit_len_i ==  length_send_cnt);
 assign          to_spi_tx_idle     = (tx_ctrl_fsm_cs == SPI_TX_EOT);
 
@@ -74,6 +84,17 @@ always@(ctrl_fsm_cs or to_ctrl_idle or to_spi_tx_transmit or to_spi_tx_eot ) beg
                                     tx_ctrl_fsm_ns <= #DLY SPI_TX_IDLE;
                                 end
                             end
+
+        // SPI tx ctrl ready data status
+        SPI_TX_READY:       begin
+                                if(to_spi_tx_ready) begin
+                                    tx_ctrl_fsm_ns <= #DLY SPI_TX_TRANSMIT;
+                                end
+                                else begin
+                                    tx_ctrl_fsm_ns <= #DLY SPI_TX_READY;
+                                end
+                            end
+
         // SPI tx Transmit status
         SPI_TX_TRANSMIT:    begin
                                 if(to_spi_tx_eot) begin
@@ -84,6 +105,7 @@ always@(ctrl_fsm_cs or to_ctrl_idle or to_spi_tx_transmit or to_spi_tx_eot ) beg
                                 end
                             end
 
+        // SPI tx end of once transmit
         SPI_TX_EOT:         begin
                                 if(to_spi_tx_transmit) begin
                                     tx_ctrl_fsm_ns <= #DLY SPI_TX_IDLE;
@@ -95,10 +117,45 @@ always@(ctrl_fsm_cs or to_ctrl_idle or to_spi_tx_transmit or to_spi_tx_eot ) beg
     endcase
 end
 
-// TX-FSM output
+// TX-FSM output , read fifo data
+always@(posedge clk_i or negedge rstn_i) begin
+    if(!rstn_i) begin
+        rd_en_r <= #DLY 1'b0;
+    end
+    else if(tx_ctrl_fsm_cs == SPI_TX_IDLE && tx_ctrl_fsm_ns == SPI_TX_READY) begin
+        rd_en_r <= #DLY 1'b1;
+    end
+    else begin
+        rd_en_r <= #DLY 1'b0;
+    end
+end
 
+always@(posedge clk_i or negedge rstn_i) begin
+    if(!rstn_i) begin
+        rd_data_r  <= #DLY 'd0;
+    end
+    else if(tx_ctrl_fsm_cs == SPI_TX_READY && tx_ctrl_fsm_ns == SPI_TX_TRANSMIT) begin
+        rd_data_r  <= #DLY rd_data_o;
+    end
+end
 
+// TX-FSM output , call spi tx to transmit
+always@(posedge clk_i or negedge rstn_i) begin
+    if(!rstn_i) begin
+        tx_vld_r <= #DLY 1'b0;
+        tx_data_r <= #DLY 'd0;
+    end
+    else if(tx_ctrl_fsm_cs == SPI_TX_TRANSMIT && tx_ctrl_fsm_ns == SPI_TX_TRANSMIT) begin
+        tx_vld_r <= #DLY 1'b0;
+        tx_data_r <= #DLY tx_data_r;
+    end
+    else begin
+        tx_vld_r <= #DLY 1'b0;
+        tx_data_r <= #DLY 'd0;
+    end
+end
 
+// TX-FSM output, transmit length sum
 
 // Instance TX sync fifo
 sync_fifo                       #(
