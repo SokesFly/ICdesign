f890da3bfa2f47e5981b0ccac06f6681e91fae61
remo <remo@localhost.localdomain>
2023-03-06 10:24:04 +0800

    add vimrc

--- systemverilog/rtl/reset_sync.sv
+++ systemverilog/rtl/reset_sync.sv
@@ -1,30 +1,28 @@
-module reset_sync(
-    input wire clk_i,
-    input wire rstn_i,
-    input wire unsynced_i,
+module reset_sync (
+    input  wire clk_i,
+    input  wire rstn_i,
+    input  wire unsynced_i,
     output wire synced_o
 );
-reg delay;
-reg synced_reg;
+  reg delay;
+  reg synced_reg;
 
-assign synced_o = synced_reg;
+  assign synced_o = synced_reg;
 
-always @(posedge clk_i or negedge rstn_i) begin
-    if(!rstn_i) begin
-        delay <= 1'b0;
-    end 
-    else begin
-        delay <= unsynced_i;
+  always @(posedge clk_i or negedge rstn_i) begin
+    if (!rstn_i) begin
+      delay <= 1'b0;
+    end else begin
+      delay <= unsynced_i;
     end
-end
+  end
 
-always @(posedge clk_i or negedge rstn_i) begin
-    if(!rstn_i) begin
-        synced_reg <= 1'b0;
+  always @(posedge clk_i or negedge rstn_i) begin
+    if (!rstn_i) begin
+      synced_reg <= 1'b0;
+    end else begin
+      synced_reg <= delay;
     end
-    else begin
-        synced_reg <= delay;
-    end
-end
+  end
 
 endmodule
--- systemverilog/tb/tb.sv
+++ systemverilog/tb/tb.sv
@@ -1,44 +1,44 @@
 module tb;
-    reg clk_i;
-    reg rstn_i;
-    reg unsynced_i;
-    reg synced_o;
+  reg             clk_i;
+  reg             rstn_i;
+  reg             unsynced_i;
+  reg             synced_o;
 
-    byte data_byte = "D";
-    bit[7:0]    msg_bit_one = "C";
+  byte            data_byte = "D";
+  bit    [   7:0] msg_bit_one = "C";
 
-    bit [8*17:1]   msg_bit = "";
-    string         msg_str = "String";
+  bit    [8*17:1] msg_bit = "";
+  string          msg_str = "String";
 
-    reset_sync dut(
-        .clk_i(clk_i),
-        .rstn_i(rstn_i),
-        .unsynced_i(unsynced_i),
-        .synced_o(synced_o)
-    );
+  reset_sync dut (
+      .clk_i(clk_i),
+      .rstn_i(rstn_i),
+      .unsynced_i(unsynced_i),
+      .synced_o(synced_o)
+  );
 
-    initial begin
-        clk_i = 1'b1;
-        forever begin
-            #5 clk_i <= ~clk_i;
-        end
+  initial begin
+    clk_i = 1'b1;
+    forever begin
+      #5 clk_i <= ~clk_i;
     end
+  end
 
-    initial begin
-        #10 rstn_i = 1'b0;
-        #10 rstn_i = 1'b1;
-    end
+  initial begin
+    #10 rstn_i = 1'b0;
+    #10 rstn_i = 1'b1;
+  end
 
-    //End simulation
-    initial begin
-        $display(msg_str,"");
-        #1000 $finish;
-    end
+  //End simulation
+  initial begin
+    $display(msg_str, "");
+    #1000 $finish;
+  end
 
-    initial begin
-        $fsdbDumpfile("tb.fsdb");
-        $fsdbDumpvars(0,"+all");
-    end
+  initial begin
+    $fsdbDumpfile("tb.fsdb");
+    $fsdbDumpvars(0, "+all");
+  end
 
 
 endmodule
